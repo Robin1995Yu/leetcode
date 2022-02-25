0fa5ae6aba9bd3197c1350c3bc5b430c4c67a94d
Robin1995Yu <robinyuchina@icloud.com>
2022-02-25 14:54:18 +0800

    13

---
+++ src/com/zhuolu/leetcode/solution/Solution13.java
@@ -0,0 +1,41 @@
+package com.zhuolu.leetcode.solution;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode-cn.com/problems/roman-to-integer/
+ * @author zhuolu
+ */
+public class Solution13 {
+    Map<Character, Integer> m = new HashMap<Character, Integer>() {
+        {
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }
+    };
+    public int romanToInt(String s) {
+        int result = 0;
+        result += m.get(s.charAt(s.length() - 1));
+        for (int i = 0; i < s.length() - 1; i++) {
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(i + 1);
+            int i1 = m.get(c1);
+            int i2 = m.get(c2);
+            if (i2 > i1) {
+                i1 = -i1;
+            }
+            result += i1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution13().romanToInt("III"));
+    }
+}
